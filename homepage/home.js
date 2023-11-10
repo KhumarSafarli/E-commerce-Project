3520dd570bdb4736bb4746ebfc9c689a318ea083
Khumar <khumarns@code.edu.az>
2023-11-10 23:45:55 +0400

    created product elements in javascript with fillProducts function

---
+++ homepage/home.js
@@ -0,0 +1,100 @@
+const productContainerElement = document.querySelector(".products-list");
+
+
+function fillProducts(products) {
+    products.forEach((product) => {
+   
+        const productElement = document.createElement("div");
+        productElement.className = "product";
+     
+        const productImageContainer = document.createElement("div");
+        productImageContainer.className = "product-image";
+      
+        const hotItemContainer = document.createElement("div");
+        hotItemContainer.className = "hot-item";
+        const hotItemText = document.createElement("h5");
+        hotItemText.textContent = "HOT";
+        hotItemContainer.appendChild(hotItemText);
+
+     
+        const imageElement = document.createElement("img");
+        imageElement.src = product.image;
+
+      
+        const hoverLayer = document.createElement("div");
+        hoverLayer.className = "hover-layer";
+
+        const actionButtonsContainer = document.createElement("div");
+        actionButtonsContainer.className = "action-buttons";
+
+    
+        const heartButton = document.createElement("button");
+        const heartIcon = document.createElement("img");
+        heartIcon.src = "./svg-icons/heart.svg";
+        heartButton.appendChild(heartIcon);
+
+        
+        const basketButton = document.createElement("button");
+        const basketIcon = document.createElement("img");
+        basketIcon.src = "./svg-icons/basket.svg";
+        basketButton.appendChild(basketIcon);
+
+        actionButtonsContainer.appendChild(heartButton);
+        actionButtonsContainer.appendChild(basketButton);
+
+        hoverLayer.appendChild(actionButtonsContainer);
+
+        productImageContainer.appendChild(hotItemContainer);
+        productImageContainer.appendChild(imageElement);
+        productImageContainer.appendChild(hoverLayer);
+
+    
+        const productDescriptionContainer = document.createElement("div");
+        productDescriptionContainer.className = "product-description";
+
+       
+        const productNameContainer = document.createElement("div");
+        productNameContainer.className = "product-name";
+        const productName = document.createElement("h4");
+        productName.textContent = product.title;
+        productNameContainer.appendChild(productName);
+
+       
+        const productRateContainer = document.createElement("div");
+        productRateContainer.className = "product-rate";
+        const productRateIcon = document.createElement("img");
+        productRateIcon.src = "./svg-icons/rating.svg";
+        productRateContainer.appendChild(productRateIcon);
+
+     
+        const productPriceContainer = document.createElement("div");
+        productPriceContainer.className = "product-price";
+
+        const newPriceElement = document.createElement("p");
+        newPriceElement.className = "new-price";
+        newPriceElement.textContent = `$ ${product.price}`;
+
+
+        const initialPriceElement = document.createElement("p");
+        initialPriceElement.className = "initial-price";
+        initialPriceElement.textContent = "$534,33"; 
+
+        const discountElement = document.createElement("p");
+        discountElement.className = "discount";
+        discountElement.textContent = "24% Off"; 
+
+        productPriceContainer.appendChild(newPriceElement);
+        productPriceContainer.appendChild(initialPriceElement);
+        productPriceContainer.appendChild(discountElement);
+
+        productDescriptionContainer.appendChild(productNameContainer);
+        productDescriptionContainer.appendChild(productRateContainer);
+        productDescriptionContainer.appendChild(productPriceContainer);
+
+        productElement.appendChild(productImageContainer);
+        productElement.appendChild(productDescriptionContainer);
+
+        productContainerElement.appendChild(productElement);
+    });
+}
+fillProducts(products);
