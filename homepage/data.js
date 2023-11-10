da3b9d2d2495813e4de5ce949ba17807e6002b63
Khumar <khumarns@code.edu.az>
2023-11-10 23:45:00 +0400

    created products data list in javascript

---
+++ homepage/data.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+      id: 1,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/sneaker.png"
+    },
+    {
+      id: 2,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/blue-shoe.png"
+    },
+    {
+      id: 3,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/yellow-sneaker.png"
+    },
+    {
+      id: 4,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/wallet.png"
+    },
+    {
+      id: 5,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/bag.png"
+    },
+    {
+      id: 6,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/backside-sneaker.png"
+    },
+    {
+      id: 7,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/foot.png"
+    },
+    {
+      id: 8,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/sneaker.png"
+    },
+    {
+      id: 9,
+      title: "Nike Air Max 270 React",
+      price: 299.43,
+      image: "../assets/images/bag.png"
+    },
+]
