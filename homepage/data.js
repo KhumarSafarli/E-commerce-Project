894914c881290fa3a9d1f517de387ce34da3662d
Khumar <khumarns@code.edu.az>
2023-11-12 01:56:47 +0400

    modified product datas

--- homepage/data.js
+++ homepage/data.js
@@ -3,7 +3,8 @@ const products = [
       id: 1,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/sneaker.png",
      
     },
@@ -11,7 +12,8 @@ const products = [
       id: 2,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/blue-shoe.png"
       
     },
@@ -19,49 +21,56 @@ const products = [
       id: 3,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/yellow-sneaker.png"
     },
     {
       id: 4,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/wallet.png"
     },
     {
       id: 5,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/bag.png"
     },
     {
       id: 6,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/backside-sneaker.png"
     },
     {
       id: 7,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/foot.png"
     },
     {
       id: 8,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/sneaker.png"
     },
     {
       id: 9,
       title: "Nike Air Max 270 React",
       price: 299.43,
-      quantity: 1,
+      newPrice: 0,
+      // quantity: 1,
       image: "../assets/images/bag.png"
     },
 ]
